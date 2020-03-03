c1622afbe3ebd2e18d3588e667a2040fcbf1b7b1
mehtadhiraj <mehtadhiraj21@gmail.com>
2020-03-03 23:38:00 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'first-angular';
+  title = "Dhiraj Mehta's First Angular App.";
+  text : string = "Hello";
+  textBox = "Text Here";
+  ngifExample : boolean = true;
+  dataArray = [
+    {
+      name: "Dhiraj",
+      branch: "IT",
+      sem: "7"
+    },
+    {
+      name: "Pallavi",
+      branch: "EXTC",
+      sem: "3"
+    }
+  ];
+  buttonClick =(e: Event)=>{
+    this.text = "Hello Dhiraj";
+    this.ngifExample = false;
+    console.log(e);  
+  }
+
+  onChange(e:Event){
+    this.textBox = "Hi Pallavi !!";
+  }
 }
